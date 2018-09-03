3c15add4cd987253822b38d2f70a4bcb7a24fd93
Ashish Vegaraju <ashish_vegaraju@yahoo.com>
2018-09-03 23:29:04 +0200

    implement then attribute

---
+++ src/XBehavior.Core/Attributes/ThenAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+using Xunit;
+
+namespace XBehavior.Core.Attributes
+{
+    public class ThenAttribute: FactAttribute
+    {
+        private readonly string _description;
+        public int Order { get; }
+        public override string DisplayName => $"Then {_description}";
+        public override string Skip => string.Empty;
+
+        public ThenAttribute(string description, int order)
+        {
+            if (string.IsNullOrEmpty(description))
+                throw new ArgumentException($"{nameof(description)} is not valid.");
+
+            Order = order;
+            _description = description;
+        }
+    }
+}
---
+++ tests/XBehavior.Core.Tests/Attributes/ThenAttributeTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+using FluentAssertions;
+
+using XBehavior.Core.Attributes;
+
+using Xunit;
+
+namespace XBehavior.Core.Tests.Attributes
+{
+    public class ThenAttributeTests
+    {
+        [Fact]
+        public void ThenAttribute_IsOfTypeFactAttribute()
+        {
+            ThenAttribute sut = new ThenAttribute("test", 1);
+            sut.GetType().BaseType.Name.Should().Be(nameof(FactAttribute));
+        }
+
+        [Fact]
+        public void ThenAttribute_AllowsToSpecifyOrderOfExecution()
+        {
+            int expectedOrder = 1;
+            ThenAttribute sut = new ThenAttribute("test", expectedOrder);
+            sut.Order.Should().Be(expectedOrder);
+        }
+
+        [Fact]
+        public void ThenAttribute_SetsCorrectDescriptionAsDisplayName()
+        {
+            string description = "attribute description.";
+            ThenAttribute sut = new ThenAttribute(description, 1);
+            sut.DisplayName.Should().Be($"Then {description}");
+        }
+
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ThenAttribute_WithDescriptionNameAsIndicated_ThrowsException(string description)
+        {
+            Action exceptionThrowingAction = () => new ThenAttribute(description, 1);
+            exceptionThrowingAction.Should().Throw<ArgumentException>()
+                                   .Which.Message.Should().Contain("description is not valid.");
+        }
+
+        [Fact]
+        public void ThenAttribute_SkipDescriptionIsAlwaysEmpty()
+        {
+            ThenAttribute sut = new ThenAttribute("test", 1);
+            sut.Skip.Should().BeEmpty();
+        }
+    }
+}
